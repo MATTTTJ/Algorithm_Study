1b8aae07fce853e9290dcbf065c711ae7f438a79
Hoon <104554976+MATTTTJ@users.noreply.github.com>
2024-10-24 19:02:18 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241024/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int n, a[10];
+
+int main() {
+    
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> a[i];
+
+    }
+
+    cout << a[2] + a[4] + a[9] << endl;
+    return 0;
+}
