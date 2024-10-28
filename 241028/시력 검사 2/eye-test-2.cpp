7acdd32d4e8ad273a678be099b16a143e1d1b44c
Hoon <104554976+MATTTTJ@users.noreply.github.com>
2024-10-28 23:02:36 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241028/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a;
+    cin >> a;
+    if(a >= 1.0f)
+    {
+        cout << "High";
+    }
+    else if ( 0.5f <=  a < 1.0f)
+    {
+        cout << "Middle";
+    }
+    else
+    cout << "Low";
+    return 0;
+}
