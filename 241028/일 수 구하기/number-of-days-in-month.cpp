62c778a76d08b44c9e8660b48d9cca732b5b7a30
Hoon <104554976+MATTTTJ@users.noreply.github.com>
2024-10-28 23:47:59 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241028/일 수 구하기/number-of-days-in-month.cpp
+++ 241028/일 수 구하기/number-of-days-in-month.cpp
@@ -9,15 +9,11 @@ int main() {
     {
         cout << "28";
     }
-    else if (n == 8)
+    else if (n == 7 || n == 8 || n ==1 || n == 3 || n == 5 || n == 10 || n == 12)
     {
         cout << "31";
     }
-    else if (n % 2 == 1)
-    {
-        cout << "31";
-    }
-    else if(n % 2 == 0)
+    else if(n == 9 || n == 11 || n == 4 || n == 6)
     {
         cout << "30";
     }
