e03e6ab208e02c71cf71246a8fecf4f1406f76dc
Hoon <104554976+MATTTTJ@users.noreply.github.com>
2024-10-28 22:58:05 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241028/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    string s = a == 100 ? "pass" : "failure";
+    
+    cout << s;
+    return 0;
+}
